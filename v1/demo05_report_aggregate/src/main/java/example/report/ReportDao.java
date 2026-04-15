375f2af5e49c0b28c5a2c1a890f79bcf4375ae88
高振中 <gzz_gzz@163.com>
2026-04-15 18:27:55 +0800

    cond.and()

--- v1/demo05_report_aggregate/src/main/java/example/report/ReportDao.java
+++ v1/demo05_report_aggregate/src/main/java/example/report/ReportDao.java
@@ -32,7 +32,7 @@ public class ReportDao extends BaseDao<Goods> {
 				SELECT u.id,u.name,COUNT(o.id) order_count,COUNT(t.id) goods_count,SUM(t.price) total_amount
 				FROM t_goods t
 				JOIN t_order o ON t.order_id = o.id
-				JOIN t_user u ON o.user_id = u.id""" + cond.where() + " GROUP BY u.id, u.name";
+				JOIN t_user u ON o.user_id = u.id WHERE t.dr IN (0,1)""" + cond.and() + " GROUP BY u.id, u.name";
 		return list(true, sql, ReportUserVo.class, cond.array());
 	}
 }
